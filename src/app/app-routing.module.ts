f3173af10463b1dd26699a4386dfe400f739eff6
Liubomyr <l.prystansky@gmail.com>
2019-08-01 17:12:03 +0300

    add cart

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
+
+const routes: Routes = [
+	{ path: '', component: ProductComponent },
+	{ path: 'products', component: ProductComponent },
+	{ path: 'cart', component: CartComponent },
+	{ path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
